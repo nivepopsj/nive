fb023eeed25d33b3c76320e12fed26b8c26ccee9
nivepopsj <43338258+nivepopsj@users.noreply.github.com>
2018-12-20 14:29:34 +0530

    Create p1.c

---
+++ p1.c
@@ -0,0 +1,12 @@
+#include<studio.h>
+#include`<conio.h>
+void main()
+{
+int a,b,c;
+printf("\n enter the value of a:");
+scanf("%d",&a);
+print("\n enter the value of b:");
+scanf("%d",&b);
+c=a+b;
+printf("\n the value is=%d",);
+}
